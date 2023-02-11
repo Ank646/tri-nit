5c7aed004fa0a36ddaa7de13f582efc2db8e4df9
fiveplusfive <125126987+fiveplusfive@users.noreply.github.com>
2023-02-11 19:20:58 +0530

    Add files via upload

--- ngoapp/views.py
+++ ngoapp/views.py
@@ -198,3 +198,39 @@ def signupuser(request):
 
 def loginuser(request):
     pass
+
+
+from django.http import FileResponse
+import io
+from reportlab.pdfgen import canvas
+from reportlab.lib.units import inch
+from reportlab.lib.pagesizes import letter
+
+def passgen(request):
+        buf=io.BytesIO()
+        c=canvas.Canvas(buf,pagesize=letter,bottomup=0)
+        textob=c.beginText()
+        textob.setTextOrigin(inch,inch)
+        textob.setFont("Helvetica",14)
+
+
+        # filter it among ngo
+        use = ngo.objects.all()
+        lines=[]
+        # Will Add All Progress Accordingly
+        for us in use:
+            lines.append(str(us.ngoname))
+            lines.append(str(us.ngocity))
+        
+        for line in lines:
+            textob.textLine(line)
+        
+        c.drawText(textob)
+        c.showPage()
+        c.save()
+        buf.seek(0)
+
+        return FileResponse(buf,as_attachment=True,filename='ngo.pdf')
+
+
+
